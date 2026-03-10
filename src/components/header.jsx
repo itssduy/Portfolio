31f75d7d8bf82ccd4bcfbb4b8257c89b38b42a37
Duy <duypersonal512@gmail.com>
2026-03-09 21:29:26 -0500

    Add Header

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import "../styles/header.css"
+
+const Header = ()=>{
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>home</li>
+                    <li>projects</li>
+                    <li>blog</li>
+                    <li>contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
