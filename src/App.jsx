02b870fe758073defd422cf85f5c01e379c73f78
Duy <duyatran05@gmail.com>
2026-04-15 20:47:00 -0500

    Wire up page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { useEffect } from 'react'
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/header'
-import './App.css'
+import Hero from './components/hero'
+import Projects from './components/projects'
+import Skills from './components/skills'
+import Experience from './components/experience'
+import NotFound from './components/not-found'
+import Footer from './components/footer'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
 
 function App() {
-
   return (
-    <>
-      <Header/>
-    </>
+    <HashRouter>
+      <ScrollToTop />
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </HashRouter>
   )
 }
 
